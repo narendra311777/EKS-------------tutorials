b1b8ca91f065fcd8613437f3fc078f54be82e57a
Anton Putra <me@antonputra.com>
2021-08-01 10:20:47 -0700

    Update app.js

--- lessons/077/secret-access/app.js
+++ lessons/077/secret-access/app.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk'),
     region = "us-east-1",
-    secretName = "prod/slack-bot/token-v1";
+    secretName = "prod/slack-bot/token-v2";
 
 const client = new AWS.SecretsManager({
     region: region
